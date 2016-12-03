25abd08a6cd75019be3b795c0db4b5545ea1d890
Dave <dphinney@salesforce.com>
2016-12-02 18:19:36 -0800

    Fix docker registry auth

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name pywebapp -p 8080:8080 --detach 254682475903.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp
+docker run --name pywebapp -p 80:8080 --detach 254682475903.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp
