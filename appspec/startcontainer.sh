fb3801e7e06dba0c223bdb0bada811fe2c5f3c9b
Dave <dphinney@salesforce.com>
2016-12-03 23:48:13 -0800

    move docker login to startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#$(aws ecr get-login --region us-west-2 | sed 's,https://,,')
+echo "home = $HOME id = $(id)"
+$(aws ecr get-login --region us-west-2)
 docker run --name pywebapp -p 80:8080 --detach 254682475903.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp
