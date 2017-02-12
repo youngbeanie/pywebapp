51634e1d2fe229377d6610e0a1f17f94dd7304dd
SatricalPine <riddell.kevin@gmail.com>
2017-02-12 00:21:22 -0800

    Update startcontainer.sh
    Updating ECR docker image again.

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -5,4 +5,4 @@ docker pull 203417976784.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp:lates
     sleep 600
     exit 1
 }
-docker run --name pywebapp -p 80:8080 --detach 254682475903.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp:latest
+docker run --name pywebapp -p 80:8080 --detach 203417976784.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp:latest
